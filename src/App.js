770dfb770a00c13a90345db8bd4836ff6a8fa207
J-Waz <jgw.jakegeorge@gmail.com>
2021-04-17 15:15:02 -0500

    adding comments and adding text to welcome page

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/* Testing Comments and changing welcome page! */}
         <p>
-          John says Hello!
+          John and Jacob say hello!
         </p>
         <a
           className="App-link"
